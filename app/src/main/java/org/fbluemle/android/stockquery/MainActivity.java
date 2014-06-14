c49629a16a202082a54f7a681d60d3a6e5f9e3c5
Frieder Bluemle <frieder.bluemle@gmail.com>
2014-06-14 14:48:37 -0700

    Rename MyActivity to MainActivity

--- app/src/main/java/org/fbluemle/android/stockquery/MainActivity.java
+++ app/src/main/java/org/fbluemle/android/stockquery/MainActivity.java
@@ -6,7 +6,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class MyActivity extends Activity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {

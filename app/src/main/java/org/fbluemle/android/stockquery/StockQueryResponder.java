fcd6578393c6981654ff10e4100837616da8a14f
Frieder Bluemle <frieder.bluemle@gmail.com>
2014-06-14 17:16:40 -0700

    Add more company name to symbol converters

--- app/src/main/java/org/fbluemle/android/stockquery/StockQueryResponder.java
+++ app/src/main/java/org/fbluemle/android/stockquery/StockQueryResponder.java
@@ -8,6 +8,12 @@ public class StockQueryResponder {
     private String getSymbol(String input) {
         if ("google".equalsIgnoreCase(input)) {
             return "GOOG";
+        } else if ("amazon".equalsIgnoreCase(input)) {
+            return "AMZN";
+        } else if ("apple".equalsIgnoreCase(input)) {
+            return "AAPL";
+        } else if ("microsoft".equalsIgnoreCase(input)) {
+            return "MSFT";
         } else {
             return input.toUpperCase();
         }

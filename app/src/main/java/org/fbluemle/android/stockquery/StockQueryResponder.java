07f9176e7c3412c1075792bd9d4ece62a83cfd45
Frieder Bluemle <frieder.bluemle@gmail.com>
2014-06-14 17:07:15 -0700

    Remove mock data

--- app/src/main/java/org/fbluemle/android/stockquery/StockQueryResponder.java
+++ app/src/main/java/org/fbluemle/android/stockquery/StockQueryResponder.java
@@ -2,16 +2,7 @@ package org.fbluemle.android.stockquery;
 
 public class StockQueryResponder {
     public String query(String input) {
-        String symbol = getSymbol(input);
-        if ("GOOG".equals(symbol)) {
-            return "551.76 +0.41 (0.07%)";
-        } else if ("EBAY".equals(symbol)) {
-            return "49.04 +0.48 (0.99%)";
-        } else if ("AAPL".equals(symbol)) {
-            return "91.28 -1.01 (-1.09%)";
-        } else {
-            return "Unknown symbol: " + symbol;
-        }
+        return getSymbol(input);
     }
 
     private String getSymbol(String input) {
